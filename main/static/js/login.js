1bc973ec54843dc0419b9cd6cb82f6f2ea189377
momazia@gmail.com <momazia@gmail.com>
2016-05-03 09:45:30 -0700

    Fixed empty string validation

--- main/static/js/login.js
+++ main/static/js/login.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
-    $("#username").on("focusout", function() {
-        var email = $(this).val();
-        console.log("Email: " + email);
-        var isEmail = validateEmail(email);
-        console.log("IsEmail: " + typeof isEmail);
-        if (!isEmail) {
-            console.log("here 1!!");
-            $(".errorMessage").text("Username not valid");
-            $(".errorMessage").show();
-            $('.login').bind('click', function(e){
-                e.preventDefault();
-            })
-        } else {
-            $(".errorMessage").hide();
-            $(".login").unbind('click')
-        }
-    });
+	$("#username").on("focusout", function() {
+		var email = $(this).val();
+		if (email != "") {
+			console.log("Email: " + email);
+			var isEmail = validateEmail(email);
+			console.log("IsEmail: " + typeof isEmail);
+			if (!isEmail) {
+				console.log("here 1!!");
+				$(".errorMessage").text("Username not valid");
+				$(".errorMessage").show();
+				$('.login').bind('click', function(e) {
+					e.preventDefault();
+				})
+			} else {
+				$(".errorMessage").hide();
+				$(".login").unbind('click')
+			}
+		} else {
+			$(".errorMessage").hide();
+		}
+	});
 });
 function validateEmail(email) {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email);
+	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	return regex.test(email);
 }
