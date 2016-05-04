b593e45fe885ad7fc2e630938b1af562540fe1b9
harsh91 <harsh_fresno@mail.fresnostate.edu>
2016-05-04 16:43:48 -0700

    Cleaned up the validations for profile page. Also, added the success functionality.

--- main/static/js/profile.js
+++ main/static/js/profile.js
@@ -1,49 +1,33 @@
 $(document).ready(function() {
     removeError();
-    $("#password").on("focusin", function() {
-        removeError();
-    });
     $("#password").on("focusout", function() {
-        validatePasswordNoEmail(this);
-    });
-    $("#retryPassword").on("focusin", function() {
-        validatePasswordNoEmail($("#password").val());
+        validatePassword(this);
     });
     $("#retryPassword").on("focusout", function() {
-        isPasswordSameNoEmail();
-    });
-    $("#fname").on("focusiin", function() {
-        isPasswordSameNoEmail();
+        isPasswordSame(true);
     });
     $("#fname").on("focusout", function() {
         if($(this).val() == "") showError("First Name cannot be blank");
     });
-    $("#lname").on("focusin", function() {
-        isPasswordSameNoEmail();
-        if($(this).val() == "") showError("First Name cannot be blank");
-
-    });
     $("#lname").on("focusout", function() {
         if($(this).val() == "") showError("Last Name cannot be blank");
     });
-    $("#submit").on("mousedown", function(e) {
-            validatePasswordNoEmail($("#password"));
-            if(!$(".errorMessage").is(":visible")) {
-                isPasswordSameNoEmail();
-                if(!$(".errorMessage").is(":visible")) {
-                    if($("#fname").val() == "") {
-                        showError("First Name cannot be blank");
+    $(".submit").on("mousedown", function(e) {
+        validatePassword($("#password"));
+        if(!$(".errorMessage").is(":visible")) {
+            isPasswordSame(false);
+            if (!$(".errorMessage").is(":visible")) {
+                if ($("#fname").val() == "") {
+                    showError("First Name cannot be blank");
+                } else {
+                    if ($("#lname").val() == "") {
+                        showError("Last Name cannot be blank");
                     } else {
                         removeError();
-                        if($("#lname").val() == "") {
-                            showError("Last Name cannot be blank");
-                        } else {
-                            removeError();
-                        }
                     }
                 }
             }
-
+        }
     });
     $("#resetBtn").on("click", function() {
         removeError();
