99f992848d18a1c19e7e768a6584306e426aee33
momazia@gmail.com <momazia@gmail.com>
2016-05-04 20:07:25 -0700

    Completed the gallery logic

--- handler/gallery.go
+++ handler/gallery.go
@@ -4,28 +4,56 @@ import (
 	"github.com/momazia/GoProject/log"
 	"github.com/momazia/GoProject/session"
 	"github.com/momazia/GoProject/storage"
-	"github.com/momazia/GoProject/util"
 	"html/template"
 	"net/http"
+	"strings"
 )
+
+type GalleryTemp struct {
+	Files []File
+}
+
+type File struct {
+	Name      string
+	Extension string
+}
 
 // Gallery handler
 func GalleryHandler(res http.ResponseWriter, req *http.Request) {
 
+	username := session.GetUser(req).Email
+
 	if req.Method == "POST" {
 		file, header, err := req.FormFile("file")
 		log.LogErrorWithMsg("Cannot read the file from the request", err)
-		err = storage.Store(req, session.GetUser(req).Email, header.Filename, file)
-		log.LogErrorWithMsg("Cannot store the uploaded file", err)
+		if err == nil {
+			err = storage.Store(req, username, header.Filename, file)
+			log.LogErrorWithMsg("Cannot store the uploaded file", err)
+		}
 	}
-
 	//Parsing the template
 	tpl := template.Must(template.ParseFiles("template/gallery.html"))
-	// Fetching user's data
-	pt := ProfileTemp{
-		User: util.GetUser(req),
+
+	// Getting user's list of file
+	fileList, err := storage.RetrieveFileList(req, username)
+	log.LogErrorWithMsg("Cannot get user's list of files", err)
+
+	files := createFiles(fileList)
+
+	gt := GalleryTemp{
+		Files: files,
 	}
 
-	err := tpl.Execute(res, GetAPlusTemplateHeader(req, pt))
+	err = tpl.Execute(res, GetAPlusTemplateHeader(req, gt))
 	log.LogError(err)
+}
+
+// Converts list of files into File
+func createFiles(fileList []string) []File {
+	var files []File
+	for _, file := range fileList {
+		str := strings.Split(file, ".")
+		files = append(files, File{Name: strings.Join(str[:len(str)-1], "."), Extension: str[len(str)-1]})
+	}
+	return files
 }
--- storage/storage.go
+++ storage/storage.go
@@ -6,6 +6,7 @@ import (
 	"google.golang.org/cloud/storage"
 	"io"
 	"net/http"
+	"strings"
 )
 
 const BUCKET_NAME = "gotraining-1271.appspot.com"
@@ -41,7 +42,30 @@ func Retrieve(req *http.Request, userName, fileName string) (io.ReadCloser, erro
 	return client.Bucket(BUCKET_NAME).Object(filePath).NewReader(ctx)
 }
 
+// Retrieves the list of files for the given username
+func RetrieveFileList(req *http.Request, userName string) ([]string, error) {
+	// Creating new context and client.
+	ctx := appengine.NewContext(req)
+	client, err := storage.NewClient(ctx)
+	log.LogErrorWithMsg("Cannot create a new client", err)
+	defer client.Close()
+
+	query := &storage.Query{
+		Delimiter: "/",
+		Prefix:    userName + "/",
+	}
+
+	objs, err := client.Bucket(BUCKET_NAME).List(ctx, query)
+	log.LogError(err)
+
+	var names []string
+	for _, result := range objs.Results {
+		names = append(names, strings.Split(result.Name, "/")[1])
+	}
+	return names, err
+}
+
 // Creates file path based on the user and file names
 func getFilePath(userName, fileName string) string {
-	return userName + "/" + fileName
+	return strings.ToLower(userName + "/" + fileName)
 }
